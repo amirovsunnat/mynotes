73bc9d8237054b4a06bac533aa37a9efcfbaabce
Sunnat <sunnatbekamirov22@gmail.com>
2023-09-23 00:40:15 +0500

    Display notes in the notes screen.

--- lib/screens/notes/notes_screen.dart
+++ lib/screens/notes/notes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
@@ -25,13 +26,9 @@ class _NotesScreenState extends State<NotesScreen> {
   }
 
   @override
-  void dispose() {
-    _notesService.close();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final currentTime = DateTime.now();
+    final formattedDate = DateFormat('MMMM dd, yyyy').format(currentTime);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notes"),
@@ -72,11 +69,57 @@ class _NotesScreenState extends State<NotesScreen> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
-                      return Column(
-                        children: [
-                          const Text("Waiting for all notes..."),
-                          Expanded(
-                            child: Align(
+                    case ConnectionState.active:
+                      if (snapshot.hasData) {
+                        final allNotes = snapshot.data as List<DatabaseNote>;
+                        return Column(
+                          children: [
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: allNotes.length,
+                                itemBuilder: (context, index) {
+                                  final note = allNotes[index];
+                                  return Card(
+                                    elevation: 2,
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 8, horizontal: 16),
+                                    child: ListTile(
+                                      contentPadding: const EdgeInsets.all(16),
+                                      title: Text(
+                                        note.text,
+                                        maxLines: 1,
+                                        softWrap: true,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      subtitle: Text(
+                                        'Created at: $formattedDate', // Format the current time
+                                        style:
+                                            GoogleFonts.poppins(fontSize: 14),
+                                      ),
+                                      trailing: IconButton(
+                                        icon: const Icon(
+                                          Icons.edit,
+                                          color: Colors.indigo,
+                                        ),
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                  );
+                                  // ListTile(
+                                  //   title: Text(
+                                  //     note.text,
+                                  //     maxLines: 1,
+                                  //     softWrap: true,
+                                  //     overflow: TextOverflow.ellipsis,
+                                  //   ),
+                                  // );
+                                },
+                              ),
+                            ),
+                            Align(
                               alignment: Alignment.bottomRight,
                               child: Padding(
                                 padding: const EdgeInsets.all(16),
@@ -107,9 +150,14 @@ class _NotesScreenState extends State<NotesScreen> {
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      );
+                          ],
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+
                     default:
                       return const Center(child: CircularProgressIndicator());
                   }
